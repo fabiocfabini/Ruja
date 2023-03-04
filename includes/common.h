e032fffbcd37e359c8d6d02b1ca0f363097aa31f
fabiocfabini <fabiolucas.carneiro@gmail.com>
2023-03-04 13:50:33 +0000

    Introduce NOT_IMPLEMENTED macro

--- includes/common.h
+++ includes/common.h
@@ -9,4 +9,9 @@
 #define RED     "\x1B[31m"
 #define RESET   "\x1B[0m"
 
+#define NOT_IMPLEMENTED(msg, file, line) \
+    do { \
+        fprintf(stderr, "%s:%d: %s is not implemented\n", file, line, msg); \
+    } while (0)
+
 #endif // RUJA_COMMON_H
