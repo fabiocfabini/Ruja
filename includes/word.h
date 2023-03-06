841924888b93b761d93ca0e0ac75b310bff1448e
fabiocfabini <fabiolucas.carneiro@gmail.com>
2023-03-06 00:11:52 +0000

    Introduce nan-boxing to Word struct

--- includes/word.h
+++ includes/word.h
@@ -1,9 +1,65 @@
 #ifndef RUJA_WORD_H
 #define RUJA_WORD_H
 
+#include <stdio.h>
+
 #include "common.h"
 
-typedef double Word;
+typedef uint64_t Word;
+
+// Types
+#define TYPE_NAN   0x7FF8000000000000
+#define TYPE_NIL   0x7FF9000000000000
+#define TYPE_BOOL  0x7FFA000000000000
+#define TYPE_CHAR  0x7FFB000000000000
+#define TYPE_INT   0x7FFC000000000000
+
+// Mask
+#define MASK_TYPE  0x7FFF000000000000
+#define MASK_VALUE 0x0000FFFFFFFFFFFF
+
+// Checks
+#define IS_NAN(x)   (((x) & MASK_TYPE) == TYPE_NAN)
+#define IS_NIL(x)   (((x) & MASK_TYPE) == TYPE_NIL)
+#define IS_BOOL(x)  (((x) & MASK_TYPE) == TYPE_BOOL)
+#define IS_CHAR(x)  (((x) & MASK_TYPE) == TYPE_CHAR)
+#define IS_INT(x)   (((x) & MASK_TYPE) == TYPE_INT)
+#define IS_DOUBLE(x) (((x) & TYPE_NAN) != TYPE_NAN)
+
+// Makes
+#define MAKE_NAN()     ((TYPE_NAN))
+#define MAKE_NIL()     ((TYPE_NIL))
+#define MAKE_BOOL(x)   ((TYPE_BOOL | (x)))
+#define MAKE_CHAR(x)   ((TYPE_CHAR | (x)))
+#define MAKE_INT(x)    ((TYPE_INT  | (uint32_t) (x)))
+#define MAKE_DOUBLE(x) double_to_word(x)
+
+// Gets
+#define AS_CHAR(x)  ((char) ((x) & MASK_VALUE))
+#define AS_BOOL(x)  ((bool) ((x) & MASK_VALUE))
+#define AS_INT(x)   ((int32_t) ((x) & MASK_VALUE)) 
+#define AS_DOUBLE(x) word_to_double(x)
+
+static inline Word double_to_word(double num) {
+    union {
+        uint64_t bits;
+        double num;
+    } data;
+    data.num = num;
+    return data.bits;
+}
+
+static inline double word_to_double(Word value) {
+    union {
+        uint64_t bits;
+        double num;
+    } data;
+    data.bits = value;
+    return data.num;
+}
+
+void print_word(FILE* stream, Word w, int width);
+
 typedef struct {
     size_t count;
     size_t capacity;
--- src/word.c
+++ src/word.c
@@ -4,6 +4,32 @@
 #include "../includes/word.h"
 #include "../includes/memory.h"
 
+void print_word(FILE* stream, Word w, int width) {
+    switch (w & MASK_TYPE) {
+        case TYPE_NAN:
+            fprintf(stream, "NAN");
+            break;
+        case TYPE_NIL:
+            fprintf(stream, "NIL");
+            break;
+        case TYPE_BOOL:
+            fprintf(stream, "%*s", width, AS_BOOL(w) ? "true" : "false");
+            break;
+        case TYPE_INT:
+            fprintf(stream, "%*" PRId32 "", width, AS_INT(w));
+            break;
+        case TYPE_CHAR:
+            fprintf(stream, "%*c", width, AS_CHAR(w));
+            break;
+        default:
+            if (IS_DOUBLE(w))
+                fprintf(stream, "%*lf", width, AS_DOUBLE(w));
+            else
+                fprintf(stream, "???");
+            break;
+    }
+}
+
 Words* words_new() {
     Words* words = malloc(sizeof(Words));
     if (words == NULL) {
